69fcbcc192e5545e18ed22bd3c25a995231d334a
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-09 09:28:30 +0100

    sortByValues

--- src/test/java/MapComparingTest.java
+++ src/test/java/MapComparingTest.java
@@ -1,10 +1,7 @@
 import com.google.common.collect.ImmutableSet;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -49,7 +46,7 @@ public class MapComparingTest {
         personMap.put(p3.getId(), p3);
 
         List<Integer> ids = personMap.entrySet().stream()
-                .sorted(Map.Entry.<Integer, Person>comparingByKey().reversed())
+                .sorted(Map.Entry.comparingByKey((x, y) -> Integer.compare(y, x)))
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
 
@@ -58,11 +55,41 @@ public class MapComparingTest {
 
     @Test
     public void sortByValues_compareBySetSize() {
-        new Person(1, ImmutableSet.of("A", "B", "C"));
+        Person p1 = new Person(1, ImmutableSet.of("1", "2", "3"));
+        Person p2 = new Person(2, ImmutableSet.of("1", "2", "3", "4"));
+        Person p3 = new Person(3, ImmutableSet.of("3"));
+
+        Map<Integer, Person> personMap = new HashMap<>();
+
+        personMap.put(p1.getId(), p1);
+        personMap.put(p2.getId(), p2);
+        personMap.put(p3.getId(), p3);
+
+        List<Integer> ids = personMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.comparingInt(p -> p.getHobbies().size())))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+        assertThat(ids, is(Arrays.asList(3, 1, 2)));
     }
 
     @Test
     public void sortByValues_compareBySetSize_reverse() {
-        new Person(1, ImmutableSet.of("A", "B", "C"));
+        Person p1 = new Person(1, ImmutableSet.of("1", "2", "3"));
+        Person p2 = new Person(2, ImmutableSet.of("1", "2", "3", "4"));
+        Person p3 = new Person(3, ImmutableSet.of("3"));
+
+        Map<Integer, Person> personMap = new HashMap<>();
+
+        personMap.put(p1.getId(), p1);
+        personMap.put(p2.getId(), p2);
+        personMap.put(p3.getId(), p3);
+
+        List<Integer> ids = personMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.<Person>comparingInt(p -> p.getHobbies().size()).reversed()))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+        assertThat(ids, is(Arrays.asList(2, 1, 3)));
     }
 }
